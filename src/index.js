f80252be8013f5d913aead5cf5c943d29e901c66
wangchengchen <wangchengchen@citycloud.com.cn>
2023-04-24 14:29:40 +0800

    feat(配置路由、dart-sass、normalize.css): 配置路由、dart-sass、normalize.css
    配置路由、dart-sass、normalize.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import '@/assets/style/reset.scss'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from '@/pages/Home.jsx';
-import About from '@/pages/About';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
